2523c9e80e910bca9b5aa4798a17097cd7459bef
Taiki Sugawara <buzz.taiki@gmail.com>
2021-12-09 23:35:41 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# GNOME Shell Extension Alt-Tab Move Mouse
+
+Move mouse pointer onto active window after Alt-Tab. This extension is workaround of https://gitlab.gnome.org/GNOME/mutter/-/issues/888
+
+## License
+GPLv3
---
+++ extension.js
@@ -0,0 +1,45 @@
+const { Clutter, Meta } = imports.gi;
+const { SwitcherPopup } = imports.ui.switcherPopup;
+
+class Extension {
+  constructor() {
+    this.orig_method = SwitcherPopup.prototype._finish;
+    const seat = Clutter.get_default_backend().get_default_seat();
+    this.vdevice = seat.create_virtual_device(
+      Clutter.InputDeviceType.POINTER_DEVICE
+    );
+  }
+
+  enable() {
+    const that = this;
+    SwitcherPopup.prototype._finish = function () {
+      that.move_pointer_maybe();
+      that.orig_method.apply(this, arguments);
+    };
+  }
+
+  disable() {
+    SwitcherPopup.prototype._finish = this.orig_method;
+  }
+
+  move_pointer_maybe() {
+    const window = global.display.focus_window;
+    if (!this.pointer_already_on_window(window)) {
+      const rect = window.get_frame_rect();
+      const x = rect.x + rect.width / 2;
+      const y = rect.y + rect.height / 2;
+
+      this.vdevice.notify_absolute_motion(global.get_current_time(), x, y);
+    }
+  }
+
+  pointer_already_on_window(window) {
+    const [x, y] = global.get_pointer();
+    const prect = new Meta.Rectangle({ x, y, width: 1, height: 1 });
+    return prect.intersect(window.get_frame_rect())[0];
+  }
+}
+
+function init() {
+  return new Extension();
+}
