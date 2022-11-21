be30bd4651afac1e17f2c8abc31a96a45cf45054
Lucas Emanuel Resck <41991486+lucasresck@users.noreply.github.com>
2022-11-20 23:03:03 -0600

    Update copyright notice

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
-// Copyright (C) 2021  Taiki Sugawara
+// Copyright (C) 2022  Lucas Emanuel Resck
 // Copyright (C) 2022  vakokako
+// Copyright (C) 2021  Taiki Sugawara
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
